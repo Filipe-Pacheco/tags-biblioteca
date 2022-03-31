0a61774121a35856c6a2d0c9eedd30e6c2290f4d
filipe-uninove <filipepachecosa@uni9.edu.br>
2022-03-31 13:05:40 -0300

    Commit inicial

---
+++ classes.js
@@ -0,0 +1,439 @@
+
+let lista_cursos = [
+    {
+        titulo: "Biomedicina",
+        keywords: ["Biomedicina",
+        "Anatomia",
+        'Bioestatística',
+        'Biofísica',
+        'Biologia Molecular',
+        'Bioquímica',
+        'Citologia',
+        'Ecologia',
+        'Embriologia',
+        'Epidemiologia',
+        'Farmacologia', 
+        'Fisiologia',
+        'Botânica',
+        'Saúde Pública',
+        'Histologia',
+        'Imunologia',
+        'Microbiologia',
+        'Radiobiologia',
+        'Parasitologia',
+        'Patologia',
+        'Química',
+        'Toxicologia',
+        'Zoologia',
+        'Bioinformática',
+        'Biologia Vegetal',
+        'Genética'
+    ],
+        classe: "saude",
+        valueNorm: "biomedicina",
+        
+    },
+    {
+        titulo: "Ciências Biológicas",
+        keywords: ["Ciências Biológicas"],
+        classe: "saude",
+        valueNorm: "ciencias-biologicas",
+    },
+    {
+        titulo: "Educação Física",
+        keywords: ["Educação Física"],
+        classe: "saude",
+        valueNorm: "educacao-fisica",
+    },
+    {
+        titulo: "Enfermagem",
+        keywords: ["Enfermagem"],
+        classe: "saude",
+        valueNorm: "enfermagem",
+    },
+    {
+        titulo: "Estética e Cosmética",
+        keywords: ["Estética e Cosmética"],
+        classe: "saude",
+        valueNorm: "estetica-e-cosmetica",
+    },
+    {
+        titulo: "Farmácia",
+        keywords: ["Farmácia"],
+        classe: "saude",
+        valueNorm: "farmacia",
+    },
+    {
+        titulo: "Fisioterapia",
+        keywords: ["Fisioterapia"],
+        classe: "saude",
+        valueNorm: "fisioterapia",
+    },
+    {
+        titulo: "Fonoaudiologia",
+        keywords: ["Fonoaudiologia"],
+        classe: "saude",
+        valueNorm: "fonoaudiologia",
+    },
+    {
+        titulo: "Gastronomia",
+        keywords: ["Gastronomia"],
+        classe: "saude",
+        valueNorm: "gastronomia",
+    },
+    {
+        titulo: "Medicina",
+        keywords: ["Medicina"],
+        classe: "saude",
+        valueNorm: "medicina",
+    },
+    {
+        titulo: "Medicina Veterinária",
+        keywords: ["Medicina Veterinária"],
+        classe: "saude",
+        valueNorm: "medicina-veterinaria",
+    },
+    {
+        titulo: "Nutrição",
+        keywords: ["Nutrição"],
+        classe: "saude",
+        valueNorm: "nutricao",
+    },
+    {
+        titulo: "Odontologia",
+        keywords: ["Odontologia"],
+        classe: "saude",
+        valueNorm: "odontologia",
+    },
+    {
+        titulo: "Psicologia",
+        keywords: ["Psicologia"],
+        classe: "saude",
+        valueNorm: "psicologia",
+    },
+    {
+        titulo: "Radiologia",
+        keywords: ["Radiologia"],
+        classe: "saude",
+        valueNorm: "radiologia",
+    },
+    {
+        titulo: "Serviço Social",
+        keywords: ["Serviço Social"],
+        classe: "saude",
+        valueNorm: "servico-social",
+    },
+    {
+        titulo: "Engenharia Civil",
+        keywords: ["Engenharia Civil", "construção civil", "construções", "engenheiro civil", "BIM"],
+        classe: "engenharia",
+        valueNorm: "engenharia-civil",
+    },
+    {
+        titulo: "Engenharia de Produção",
+        keywords: ["Engenharia de Produção"],
+        classe: "engenharia",
+        valueNorm: "engenharia-de-producao",
+    },
+    {
+        titulo: "Engenharia Elétrica",
+        keywords: ["Engenharia Elétrica"],
+        classe: "engenharia",
+        valueNorm: "engenharia-eletrica",
+    },
+    {
+        titulo: "Engenharia Mecânica",
+        keywords: ["Engenharia Mecânica"],
+        classe: "engenharia",
+        valueNorm: "engenharia-mecanica",
+    },
+    {
+        titulo: "Mecatrônica",
+        keywords: ["Mecatrônica"],
+        classe: "engenharia",
+        valueNorm: "mecatronica-industrial",
+    },
+    {
+        titulo: "Análise e Desenvolvimento de Sistemas",
+        keywords: ["Análise e Desenvolvimento de Sistemas", "Desenvolvimento de Sistemas", "Análise de Sistemas"],
+        classe: "engenharia",
+        valueNorm: "analise-e-desenvolvimento-de-sistemas",
+    },
+    {
+        titulo: "Banco de Dados",
+        keywords: ["Banco de Dados"],
+        classe: "informatica",
+        valueNorm: "banco-de-dados",
+    },
+    {
+        titulo: "Ciência da Computação",
+        keywords: [
+            "Ciência da Computação",
+            "Computação",
+            "sistemas de informática0",
+            "redes corporativas",
+            "arquitetura de software",
+            "cientista da computação",
+            "cientistas da computação",
+            "Programação",
+            "Projeto de Algoritmos",
+            "Projeto de Software",
+            "Algoritmos e Programação",
+            "Projeto de Algoritmos",
+            "Projeto de Software",
+            "Organização de Computadores",
+            "Arquitetura de Computadores",
+            "Banco de Dados",
+            "Circuitos Digitais",
+            "Compiladores",
+            "Computação Científica",
+            "Computação Gráfica",
+            "Computação Simbólica",
+            "Computação  Numérica",
+            "Data Science",
+            "Desenvolvimento de Games",
+            "Desenvolvimento Web e Mobile",
+            "Engenharia de Software",
+            "Estatística Computacional",
+            "Probabilidade Computacional",
+            "Estrutura de Dados",
+            "Tecnologia da Informação",
+            "Inteligência Artificial",
+            "Introdução à Computação",
+            "Engenharia de Software",
+            "Linguagens de Programação",
+            "Autômatos",
+            "Álgebra Booleana",
+            "Matemática Discreta",
+            "Modelagem de Sistemas",
+            "Simulação de Sistemas",
+            "Processamento Digital",
+            "Programação Orientada a Objetos",
+            "Programação Paralela",
+            "Programação Distribuída",
+            "Redes de Computadores",
+            "Segurança de Sistemas",
+            "Sistemas Distribuídos",
+            "Sistemas Operacionais",
+            "Tecnologias para Gestão de Dados",
+            "Teoria da Computação",
+            "Teste de Software",
+            "Computação Aplicada",
+            "Álgebra Linear",
+        ],
+        classe: "informatica",
+        valueNorm: "ciencia-da-computacao",
+    },
+    {
+        titulo: "Informática",
+        keywords: ["Informática"],
+        classe: "informatica",
+        valueNorm: "informatica",
+    },
+    {
+        titulo: "Jogos Digitais",
+        keywords: ["Jogos Digitais"],
+        classe: "informatica",
+        valueNorm: "jogos-digitais",
+    },
+    {
+        titulo: "Redes de Computadores",
+        keywords: ["Redes de Computadores"],
+        classe: "informatica",
+        valueNorm: "redes-de-computadores",
+    },
+    {
+        titulo: "Segurança da Informação",
+        keywords: ["Segurança da Informação"],
+        classe: "informatica",
+        valueNorm: "seguranca-da-informacao",
+    },
+    {
+        titulo: "Administração",
+        keywords: ["Administração"],
+        classe: "gerenciais",
+        valueNorm: "administracao",
+    },
+    {
+        titulo: "Ciências Contábeis e Econômicas",
+        keywords: ["Ciências Contábeis e Econômicas", "Economia", "Econômica"],
+        classe: "gerenciais",
+        valueNorm: "ciencias-contabeis",
+    },
+    {
+        titulo: "Comércio Exterior",
+        keywords: ["Comércio Exterior"],
+        classe: "gerenciais",
+        valueNorm: "comercio-exterior",
+    },
+    {
+        titulo: "Gestão Comercial",
+        keywords: ["Gestão Comercial"],
+        classe: "gerenciais",
+        valueNorm: "gestao-comercial",
+    },
+    {
+        titulo: "Gestão da Qualidade",
+        keywords: ["Gestão da Qualidade"],
+        classe: "gerenciais",
+        valueNorm: "gestao-da-qualidade",
+    },
+    {
+        titulo: "Gestão da Tecnologia da Informação",
+        keywords: ["Gestão da Tecnologia da Informação", "Tecnologia da Informação"],
+        classe: "gerenciais",
+        valueNorm: "gestao-da-tecnologia-da-informacao",
+    },
+    {
+        titulo: "Gestão de Recursos Humanos",
+        keywords: ["Gestão de Recursos Humanos", "Recursos Humanos"],
+        classe: "gerenciais",
+        valueNorm: "gestao-de-recursos-humanos",
+    },
+    {
+        titulo: "Gestão Financeira",
+        keywords: ["Gestão Financeira"],
+        classe: "gerenciais",
+        valueNorm: "gestao-financeira",
+    },
+    {
+        titulo: "Gestão Hospitalar",
+        keywords: ["Gestão Hospitalar"],
+        classe: "gerenciais",
+        valueNorm: "gestao-hospitalar",
+    },
+    {
+        titulo: "Logística",
+        keywords: ["Logística"],
+        classe: "gerenciais",
+        valueNorm: "logistica",
+    },
+    {
+        titulo: "Marketing",
+        keywords: ["Marketing"],
+        classe: "gerenciais",
+        valueNorm: "marketing",
+    },
+    {
+        titulo: "Turismo",
+        keywords: ["Turismo"],
+        classe: "gerenciais",
+        valueNorm: "turismo",
+    },
+    {
+        titulo: "Arquitetura e Urbanismo",
+        keywords: ["Arquitetura e Urbanismo", "Urbanismo"],
+        classe: "arquitetura-e-design",
+        valueNorm: "arquitetura-e-urbanismo",
+    },
+    {
+        titulo: "Design",
+        keywords: ["Design Gráfico", "Design de produto", "Design de produtos", "comunicação visual", " identidade visual", "tipografia", "interfaces gráficas", "interface gráfica", "experiência do usuário"],
+        classe: "arquitetura-e-design",
+        valueNorm: "design",
+    },
+    {
+        titulo: "Direito",
+        keywords: ["Direito"],
+        classe: "juridicas",
+        valueNorm: "direito",
+    },
+    {
+        titulo: "Comunicação Institucional",
+        keywords: ["Comunicação Institucional"],
+        classe: "comunicacao",
+        valueNorm: "comunicacao-institucional",
+    },
+    {
+        titulo: "Fotografia",
+        keywords: ["Fotografia"],
+        classe: "comunicacao",
+        valueNorm: "fotografia",
+    },
+    {
+        titulo: "Jornalismo",
+        keywords: ["Jornalismo"],
+        classe: "comunicacao",
+        valueNorm: "jornalismo",
+    },
+    {
+        titulo: "Produção Audiovisual",
+        keywords: ["Produção Audiovisual"],
+        classe: "comunicacao",
+        valueNorm: "producao-audiovisual",
+    },
+    {
+        titulo: "Publicidade e Propaganda",
+        keywords: ["Publicidade", "Propaganda"],
+        classe: "comunicacao",
+        valueNorm: "publicidade-e-propaganda",
+    },
+    {
+        titulo: "Rádio e TV",
+        keywords: ["Rádio e TV", "Rádio", "tv"],
+        classe: "comunicacao",
+        valueNorm: "radio-e-tv",
+    },
+    {
+        titulo: "Pedagogia",
+        keywords: ["Pedagogia", "educação"],
+        classe: "educacao",
+        valueNorm: "pedagogia",
+    },
+];
+
+
+let count = 0
+
+
+function verificaClasse(livro){
+    // Obtendo a descrição e o titulo e transfomando para maiúsculo, sem acento e pontuação.
+    let descricao_livro = livro.Descricao.toLocaleUpperCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    let titulo_livro = livro.title.toLocaleUpperCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    // console.log('----------------------Analisando o livro: ' + titulo_livro)
+
+    // ARRAY DE TAGS ANALISADAS
+    let tags_analisadas = []
+
+    for (i in lista_cursos){
+        let curso = lista_cursos[i];
+        // console.log('Analisando semelhança para ' + curso.titulo);
+
+        // Loop dentro das keywords
+        for (y in curso.keywords){
+            // keyword em análise
+            let keywordNow = curso.keywords[y].toLocaleUpperCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+            //  console.log('keywordNow: ' + keywordNow)
+
+            // A descrição tem alguma das keywords?
+            // console.log('A descrição tem a keyword: ' + keywordNow + '?')
+            let descricao_contem_bool = descricao_livro.includes(keywordNow);
+            // console.log('Resposta: ' + descricao_contem_bool)
+
+            // O título tem alguma das keywords?
+            // console.log('O título tem a keyword: ' + keywordNow + '?')
+            let titulo_contem_bool = titulo_livro.includes(keywordNow);
+            // console.log('Resposta: ' + titulo_contem_bool)
+
+            // Se o título ou a descrição tem, adicione o nome do curso correspondente ao array tags_analisadas
+            if (descricao_contem_bool || titulo_contem_bool){
+                tags_analisadas.push(curso.titulo)
+
+                // Se a classe existe adicione o livro ao array, se não, crie um array no objeto "todos_resuldos" e então adicione o livro
+                if (todos_resultados[curso.titulo]){
+                    todos_resultados[curso.titulo].push({title: livro.title, descricao: livro.Descricao})
+                    
+
+                } else {
+                    todos_resultados[curso.titulo] = []
+                    todos_resultados[curso.titulo].push({title: livro.title, descricao: livro.Descricao})
+                }
+            }
+        }
+    }
+    count++
+    console.log(count)
+    livro.Tag_Descricao = []
+    livro.Tag_Descricao.push(tags_analisadas)
+    return tags_analisadas;
+}
